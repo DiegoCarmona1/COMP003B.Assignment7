38cc430075e5c6771511b8d7f853f3a5698943f9
Diego Carmona <ultgojifan@gmail.com>
2024-05-09 16:26:07 -0700

    Created secrets.json, and created WebDevAcademy context

---
+++ COMP003B.Assignment7/Data/WebDevAcademyContext.cs
@@ -0,0 +1,18 @@
+﻿using COMP003B.Assignment7.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.Assignment7.Data
+{
+    public class WebDevAcademyContext : DbContext
+    {
+        public WebDevAcademyContext(DbContextOptions<WebDevAcademyContext> options)
+            : base(options)
+        { 
+        }
+
+        public DbSet<BookModel> bookModels { get; set; }
+        public DbSet<AuthorModel> authorModels { get; set; }
+        public DbSet<BookAuthorModel> bookAuthorModels { get; set; }
+    }
+}
