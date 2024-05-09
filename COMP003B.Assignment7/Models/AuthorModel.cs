3dabb7a6da0935a922216dfd051f1b642004c75f
Diego Carmona <ultgojifan@gmail.com>
2024-05-09 16:09:58 -0700

    Added Input requirements to AuthorModel Class

--- COMP003B.Assignment7/Models/AuthorModel.cs
+++ COMP003B.Assignment7/Models/AuthorModel.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace COMP003B.Assignment7.Models
 {
     public class AuthorModel
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public int AuthorId { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<BookAuthorModel>? BookAuthorModels { get; set; }
     }
 }
